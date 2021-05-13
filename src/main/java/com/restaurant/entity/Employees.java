eba9d1c8fbb7d6c5418903e91c19d0f417b6b7e9
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-12 22:52:54 -0700

    added spring annotations data and builder to Orders, Employees, Customer, and Food classes

--- src/main/java/com/restaurant/entity/Employees.java
+++ src/main/java/com/restaurant/entity/Employees.java
@@ -1,5 +1,10 @@
 package com.restaurant.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Employees {
     private int emp_id;
     private int order_id;
--- src/main/java/com/restaurant/entity/Orders.java
+++ src/main/java/com/restaurant/entity/Orders.java
@@ -1,6 +1,10 @@
 package com.restaurant.entity;
 
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class Orders {
     private int order_id;
     private int customer_id;
