c2dea83ebe3cdde8dfed5cc77d5361713a605f56
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-12 20:10:23 -0700

    created DefaultRestaurantController and RestaurantController classes. added Spring annotation for ResponseStatus and Slf4j to DefaultRestaurantController

---
+++ src/main/java/com/restaurant/controller/DefaultRestaurantController.java
@@ -0,0 +1,8 @@
+package com.restaurant.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus @Slf4j
+public class DefaultRestaurantController {
+}
