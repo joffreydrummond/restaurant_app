ad9fee63d312a4bf228681165c0fd1903ff9bc71
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-13 15:30:36 -0700

    added SpringbootApp annotation. used SpringApplication object to verify Spring running. it is

--- src/main/java/com/bnj/restaurant/RestaurantApp.java
+++ src/main/java/com/bnj/restaurant/RestaurantApp.java
@@ -1,10 +1,12 @@
 package com.bnj.restaurant;
 
+import com.restaurant.app.ComponentScanMarker;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication(scanBasePackageClasses = {ComponentScanMarker.class})
 public class RestaurantApp {
   public static void main(String[] args) {
-    //
+    SpringApplication.run(RestaurantApp.class, args);
   }
 }
