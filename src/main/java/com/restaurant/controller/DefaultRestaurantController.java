f4f0cbbc5b40fd33a80b609b4f48194b3dd0c374
Brandelyn <brandelyns@gmail.com>
2021-05-12 20:34:34 -0700

    fixed @data in customers

--- src/main/java/com/restaurant/controller/DefaultRestaurantController.java
+++ src/main/java/com/restaurant/controller/DefaultRestaurantController.java
@@ -3,6 +3,7 @@ package com.restaurant.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus @Slf4j
+@ResponseStatus
+@Slf4j
 public class DefaultRestaurantController {
 }
