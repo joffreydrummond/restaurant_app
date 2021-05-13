9e4ad31e390e28de55291d3ed580c9483c06980e
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-13 15:27:42 -0700

    added psvm to Restaurant class

---
+++ src/main/java/com/bnj/restaurant/RestaurantApp.java
@@ -0,0 +1,10 @@
+package com.bnj.restaurant;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class RestaurantApp {
+  public static void main(String[] args) {
+    //
+  }
+}
