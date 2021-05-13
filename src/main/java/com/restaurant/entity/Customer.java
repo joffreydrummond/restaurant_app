6d4522da9ef58161a9184a7311a78f9617aa4a3e
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-12 20:18:24 -0700

    added instance variables to Customer class

--- src/main/java/com/restaurant/entity/Customer.java
+++ src/main/java/com/restaurant/entity/Customer.java
@@ -1,6 +1,9 @@
 package com.restaurant.entity;
 
 public class Customer {
-  
-
+  private String food_name;
+  private String description;
+  private boolean is_gluten_free;
+  private boolean is_vegan_free;
+  private double price;
 }
