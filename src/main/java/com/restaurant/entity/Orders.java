a087dbad251459285327786a37f4af8e5125effd
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-12 22:23:02 -0700

    added instance variables to Orders class

--- src/main/java/com/restaurant/entity/Orders.java
+++ src/main/java/com/restaurant/entity/Orders.java
@@ -2,5 +2,9 @@ package com.restaurant.entity;
 
 
 public class Orders {
-
-}
+    private int order_id;
+    private int customer_id;
+    private double price;
+    private String order_date;
+    private boolean order_filled;
+  }
