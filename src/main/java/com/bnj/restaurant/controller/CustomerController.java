1a43549daec2d5fb95c661134add518822270ed1
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-15 19:37:40 -0700

    added log.info to verify the controller is working

--- src/main/java/com/bnj/restaurant/controller/CustomerController.java
+++ src/main/java/com/bnj/restaurant/controller/CustomerController.java
@@ -2,16 +2,18 @@ package com.bnj.restaurant.controller;
 
 
 import com.bnj.restaurant.entity.Customer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController @Slf4j
 public class CustomerController implements CustomerOperation{
 
 
     @Override
     public List<Customer> getCustomers() {
+        log.info("I am in the getCustomers() in the controller");
         return null;
     }
 }
