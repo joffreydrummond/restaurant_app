dcfe763eb5a3e19cb65987a9dc514fa31c4952b1
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-15 18:13:27 -0700

    added getCustomers method to CustomerController

--- src/main/java/com/bnj/restaurant/controller/CustomerController.java
+++ src/main/java/com/bnj/restaurant/controller/CustomerController.java
@@ -1,10 +1,15 @@
 package com.bnj.restaurant.controller;
 
+import com.bnj.restaurant.entity.Customer;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
+import java.util.List;
+
 @OpenAPIDefinition(
     info = @Info(title = "Customer API"),
     servers = {@Server(url = "http://localhost:8080")})
-public interface CustomerController {}
+public interface CustomerController {
+  List<Customer> getCustomers();
+}
