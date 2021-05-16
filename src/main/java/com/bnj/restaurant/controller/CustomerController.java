ce03b14fe93cabc5b8e5d0f21df9e83926cc587a
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-15 18:38:40 -0700

    added restcontroller annotation to CustomerController

--- src/main/java/com/bnj/restaurant/controller/CustomerController.java
+++ src/main/java/com/bnj/restaurant/controller/CustomerController.java
@@ -2,9 +2,11 @@ package com.bnj.restaurant.controller;
 
 
 import com.bnj.restaurant.entity.Customer;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class CustomerController implements CustomerOperation{
 
 
