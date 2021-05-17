e8a0956df27687bde60566b4581e46a96ee3edb4
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-16 17:38:13 -0700

    added jdbc dependency

--- src/test/java/com/bnj/restaurant/controller/FetchCustomerTest.java
+++ src/test/java/com/bnj/restaurant/controller/FetchCustomerTest.java
@@ -1,6 +1,20 @@
 package com.bnj.restaurant.controller;
 
 import com.bnj.restaurant.controller.support.FetchCustomerSupport;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class FetchCustomerTest extends FetchCustomerSupport {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void testDb(){
+        int numRows = JdbcTestUtils.countRowsInTable(jd)
+    }
 }
