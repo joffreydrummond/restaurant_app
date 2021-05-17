6956fdd0e048b6b624c9fa1ef9de081d6003c6cf
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-16 17:33:05 -0700

    created getbaseurl method in basetest

--- src/test/java/com/bnj/restaurant/controller/support/BaseTest.java
+++ src/test/java/com/bnj/restaurant/controller/support/BaseTest.java
@@ -1,4 +1,17 @@
 package com.bnj.restaurant.controller.support;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
 public class BaseTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Autowired @Getter
+    private TestRestTemplate restTemplate;
+
+    protected String getBaseUrl(){return String.format("http://localhost:%d/restaurant_db", serverPort);}
 }
