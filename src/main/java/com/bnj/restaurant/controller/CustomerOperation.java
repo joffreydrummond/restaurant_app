efd525d8063d248ce41a1d08a171615fffd4741d
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-17 23:19:45 -0700

    cleaned up parameter description

--- src/main/java/com/bnj/restaurant/controller/CustomerOperation.java
+++ src/main/java/com/bnj/restaurant/controller/CustomerOperation.java
@@ -3,6 +3,7 @@ package com.bnj.restaurant.controller;
 import com.bnj.restaurant.entity.Customer;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -16,15 +17,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
-//returns list of customers
+// returns list of customers
 
 @OpenAPIDefinition(
     info = @Info(title = "Customer API V1"),
     servers = {@Server(url = "http://localhost:8080")})
 public interface CustomerOperation {
 
-    @RequestMapping("/v1/customers")
-    @Operation(
+  @RequestMapping("/v1/customers")
+  @Operation(
       summary = "Returns a list of customers",
       description = "Returns a list of customers",
       responses = {
@@ -41,19 +42,20 @@ public interface CustomerOperation {
             content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
         @ApiResponse(
             responseCode = "404",
-            description = "The page you are looking for is not found. What did you do.",
+            description = "The customer was not found.",
             content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
         @ApiResponse(
             responseCode = "500",
             description = "Internal Server Error Occurred",
             content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
-      })
-
-
-
-
-
-
+      },
+          parameters = {
+                  @Parameter(
+                          name = "customer_id",
+                          allowEmptyValue = false,
+                          required = false,
+                          description = "The customer id (i.e, '1', '2', '3')"),
+                         })
   @GetMapping
   @ResponseStatus(code = HttpStatus.OK)
   List<Customer> getCustomers();
--- src/test/java/com/bnj/restaurant/controller/FetchCustomerTest.java
+++ src/test/java/com/bnj/restaurant/controller/FetchCustomerTest.java
@@ -1,20 +1,29 @@
 package com.bnj.restaurant.controller;
 
+import com.bnj.restaurant.ActiveProfile;
 import com.bnj.restaurant.controller.support.FetchCustomerSupport;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@ActiveProfiles("test")
+//@Sql(scripts = {"classpath:data/restaurant_data.sql", "classpath:data/restaurant_schema.sql"}, config =
+//@SqlConfig(encoding = "utf-8"))
 public class FetchCustomerTest extends FetchCustomerSupport {
 
-    private JdbcTemplate jdbcTemplate;
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
-    @Test
-    void testDb(){
-        int numRows = JdbcTestUtils.countRowsInTable(jd)
-    }
+  @Test
+  void testDb() {
+    int numRows = JdbcTestUtils.countRowsInTable(jdbcTemplate, "customers");
+    System.out.println("Num: " + numRows);
+  }
 }
