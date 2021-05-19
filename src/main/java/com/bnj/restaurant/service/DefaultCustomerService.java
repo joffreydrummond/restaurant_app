333b20e062b9b4f57b5eba39a01d58e99306b4de
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-19 00:26:18 -0700

    added autowired annotation to CustomerDao interface

--- src/main/java/com/bnj/restaurant/service/DefaultCustomerService.java
+++ src/main/java/com/bnj/restaurant/service/DefaultCustomerService.java
@@ -1,14 +1,23 @@
 package com.bnj.restaurant.service;
 
+import com.bnj.restaurant.dao.CustomerDao;
 import com.bnj.restaurant.entity.Customer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Slf4j
-public class DefaultCustomerService  implements CustomerService{
-    @Override
-    public List<Customer> getCustomers( ) {
-        log.debug("I am getCustomers() in service");
-        return null;
-    }
+@Service
+public class DefaultCustomerService implements CustomerService {
+
+  @Autowired
+  private CustomerDao customerDao;
+
+  @Override
+  public List<Customer> getCustomers() {
+    log.debug("I am getCustomers() in service");
+    return null;
+  }
 }
