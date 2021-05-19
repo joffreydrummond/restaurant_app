34cef8fbe5e4bf4b8557eebef5cf269ee53ba61b
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-19 00:24:05 -0700

    added autowired annotation to CustomerService interface

--- src/main/java/com/bnj/restaurant/controller/CustomerController.java
+++ src/main/java/com/bnj/restaurant/controller/CustomerController.java
@@ -2,7 +2,9 @@ package com.bnj.restaurant.controller;
 
 
 import com.bnj.restaurant.entity.Customer;
+import com.bnj.restaurant.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,9 @@ import java.util.List;
 
 @RestController @Slf4j
 public class CustomerController implements CustomerOperation{
+
+    @Autowired
+    private CustomerService customerService;
 
     @RequestMapping
 
