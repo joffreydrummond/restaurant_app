27bb12aae9f8fedfb8b730b18a4ea94adcf3d241
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-19 00:16:43 -0700

    created DefaultCustomerDao

---
+++ src/main/java/com/bnj/restaurant/dao/DefaultCustomerDao.java
@@ -0,0 +1,12 @@
+package com.bnj.restaurant.dao;
+
+import com.bnj.restaurant.entity.Customer;
+
+import java.util.List;
+
+public class DefaultCustomerDao  implements CustomerDao {
+    @Override
+    public List<Customer> getCustomers() {
+        return null;
+    }
+}
