a1411884354fd662b2147d199b51b853ab96da91
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-19 00:31:16 -0700

    added namedparamaterjdbctemplate

--- src/main/java/com/bnj/restaurant/dao/DefaultCustomerDao.java
+++ src/main/java/com/bnj/restaurant/dao/DefaultCustomerDao.java
@@ -2,6 +2,8 @@ package com.bnj.restaurant.dao;
 
 import com.bnj.restaurant.entity.Customer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,6 +11,10 @@ import java.util.List;
 @Slf4j
 @Component
 public class DefaultCustomerDao implements CustomerDao {
+
+  @Autowired
+  private NamedParameterJdbcTemplate jdbcTemplate;
+
   @Override
   public List<Customer> getCustomers() {
     log.debug("I am getCustomers() in dao");
