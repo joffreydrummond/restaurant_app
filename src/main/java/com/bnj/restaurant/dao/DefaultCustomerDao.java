60ba374907c12d9b899b9211fdfae929e70e5f07
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-19 00:27:02 -0700

    added component annotation to DefaultCustomerDao interface

--- src/main/java/com/bnj/restaurant/dao/DefaultCustomerDao.java
+++ src/main/java/com/bnj/restaurant/dao/DefaultCustomerDao.java
@@ -2,13 +2,16 @@ package com.bnj.restaurant.dao;
 
 import com.bnj.restaurant.entity.Customer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Slf4j
-public class DefaultCustomerDao  implements CustomerDao {
-    @Override
-    public List<Customer> getCustomers() {
-        log.debug("I am getCustomers() in dao");
-        return null;
-    }
+@Component
+public class DefaultCustomerDao implements CustomerDao {
+  @Override
+  public List<Customer> getCustomers() {
+    log.debug("I am getCustomers() in dao");
+    return null;
+  }
 }
