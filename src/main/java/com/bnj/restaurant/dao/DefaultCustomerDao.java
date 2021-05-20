16bbba4c3b1a6142162dc157825ed7bf3a6d7aa7
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-19 17:35:18 -0700

    changed spring jdbc dependency to spring-boot-starter-data-jdbc

--- src/main/java/com/bnj/restaurant/dao/DefaultCustomerDao.java
+++ src/main/java/com/bnj/restaurant/dao/DefaultCustomerDao.java
@@ -3,8 +3,10 @@ package com.bnj.restaurant.dao;
 import com.bnj.restaurant.entity.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
@@ -15,29 +17,30 @@ import java.util.List;
 @Component
 public class DefaultCustomerDao implements CustomerDao {
 
-//  @Autowired
-//  private NamedParameterJdbcTemplate jdbcTemplate;
+  @Autowired
+  private NamedParameterJdbcTemplate jdbcTemplate;
 
   @Override
   public List<Customer> getCustomers() {
     log.debug("I am getCustomers() in dao");
 
     final String sql = "SELECT * FROM customers";
-    return null;
-//            jdbcTemplate.query(
-//        sql,
-//        new RowMapper<Customer>() {
-//          @Override
-//          public Customer mapRow(ResultSet rs, int i) throws SQLException {
-//            return Customer.builder()
-//                .customer_id(rs.getInt("customer_id"))
-//                .first_name(rs.getString("first_name"))
-//                .last_name(rs.getString("last_name"))
-//                .address(rs.getString("address"))
-//                .phone(rs.getString("phone"))
-//                .email(rs.getString("email"))
-//                .build();
-//          }
-//        });
+    return
+//            null;
+            jdbcTemplate.query(
+        sql,
+        new RowMapper<Customer>() {
+          @Override
+          public Customer mapRow(ResultSet rs, int i) throws SQLException {
+            return Customer.builder()
+                .customer_id(rs.getInt("customer_id"))
+                .first_name(rs.getString("first_name"))
+                .last_name(rs.getString("last_name"))
+                .address(rs.getString("address"))
+                .phone(rs.getString("phone"))
+                .email(rs.getString("email"))
+                .build();
+          }
+        });
   }
 }
