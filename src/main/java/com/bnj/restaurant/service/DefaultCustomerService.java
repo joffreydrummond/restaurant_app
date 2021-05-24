5ac4742b5e75d1baef4f890fb2d48f558582cb74
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-24 13:49:03 -0700

    added collections to sort list of customers by customer id

--- src/main/java/com/bnj/restaurant/service/DefaultCustomerService.java
+++ src/main/java/com/bnj/restaurant/service/DefaultCustomerService.java
@@ -6,22 +6,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
-
 
 @Slf4j
 @Service
 public class DefaultCustomerService implements CustomerService {
 
-  @Autowired
-  private CustomerDao customerDao;
+  @Autowired private CustomerDao customerDao;
 
   @Override
   public List<Customer> getCustomers() {
     log.debug("I am getCustomers() in service");
     List<Customer> customers = customerDao.getCustomers();
+    Collections.sort(customers);
 
-
-    return null;
+    return customers;
   }
 }
