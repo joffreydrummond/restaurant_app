974f74c3c61491e774b7fcf125791b78796f47fd
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-24 14:47:34 -0700

    created openapi docs for create customer

--- src/main/java/com/bnj/restaurant/controller/CustomerOperation.java
+++ src/main/java/com/bnj/restaurant/controller/CustomerOperation.java
@@ -57,6 +57,37 @@ public interface CustomerOperation {
   @ResponseStatus(code = HttpStatus.OK)
   List<Customer> getCustomers();
 
+    @Operation(
+            summary = "Creates a new customer",
+            description = "Creates a new customer",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "A new customer is successfully created",
+                            content =
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Customer.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "The customer parameters are not correct",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "The customer was not found.",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error Occurred",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            },
+            parameters = {
+                    @Parameter(
+                            name = "customer_id",
+                            allowEmptyValue = false,
+                            required = false,
+                            description = "The customer id (i.e, '1', '2', '3')"),
+            })
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
   Customer createCustomer(@RequestBody Customer customer);
