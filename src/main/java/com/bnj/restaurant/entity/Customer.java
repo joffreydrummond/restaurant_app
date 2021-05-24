39c2af49d0cbac0527adf67ab96c0caccf6f9b1e
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-24 13:44:15 -0700

    return Comparator comparing based on customerId so it sorts based on customer id

--- src/main/java/com/bnj/restaurant/entity/Customer.java
+++ src/main/java/com/bnj/restaurant/entity/Customer.java
@@ -4,25 +4,30 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Comparator;
 
 @Data
 @Builder
-public class Customer implements Comparable<Customer>{
+public class Customer implements Comparable<Customer> {
   private int customer_id;
 
   @NotBlank(message = "First name is required")
   private String first_name;
+
   @NotBlank(message = "Last name is required")
   private String last_name;
+
   @NotBlank(message = "Address is required")
   private String address;
+
   @NotBlank(message = "Phone number is required")
   private String phone;
+
   @NotBlank(message = "Email is required")
   private String email;
 
   @Override
-  public int compareTo(Customer o) {
-    return 0;
+  public int compareTo(Customer that) {
+    return Comparator.comparing(Customer::getCustomer_id).compare(this, that);
   }
 }
