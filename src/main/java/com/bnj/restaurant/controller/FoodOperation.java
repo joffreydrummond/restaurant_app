1b0b601714c227d8e6f7cb5c5be8dc7aa25933ab
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-27 09:54:16 -0700

    added openapidef at class level to FoodOperation

--- src/main/java/com/bnj/restaurant/controller/FoodOperation.java
+++ src/main/java/com/bnj/restaurant/controller/FoodOperation.java
@@ -1,4 +1,12 @@
 package com.bnj.restaurant.controller;
 
-public interface FoodOperation {
-}
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/v1/foods")
+@OpenAPIDefinition(
+    info = @Info(title = "Food API"),
+    servers = {@Server(url = "http://localhost:8080", description = "Local Server")})
+public interface FoodOperation {}
