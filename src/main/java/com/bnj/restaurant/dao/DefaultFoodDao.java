84f09a35a5008950444f2cb08c0020f5a4857fcf
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-27 11:12:34 -0700

    added getFoods to FoodDao. implemented method in DefaultFoodDao.

--- src/main/java/com/bnj/restaurant/dao/DefaultFoodDao.java
+++ src/main/java/com/bnj/restaurant/dao/DefaultFoodDao.java
@@ -1,6 +1,14 @@
 package com.bnj.restaurant.dao;
 
+import com.bnj.restaurant.entity.Food;
+
+import java.util.List;
+
 public class DefaultFoodDao implements FoodDao {
 
 
+    @Override
+    public List<Food> getFoods() {
+        return null;
+    }
 }
--- src/main/java/com/bnj/restaurant/dao/FoodDao.java
+++ src/main/java/com/bnj/restaurant/dao/FoodDao.java
@@ -1,4 +1,9 @@
 package com.bnj.restaurant.dao;
 
+import com.bnj.restaurant.entity.Food;
+
+import java.util.List;
+
 public interface FoodDao {
+    List<Food> getFoods();
 }
