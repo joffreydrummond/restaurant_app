0ba2c948b2e0d3f4548831262d14041a5cafb7f7
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-27 11:10:47 -0700

    added getFoods to FoodService. implemented method in DefaultFoodService. added debug log

--- src/main/java/com/bnj/restaurant/service/DefaultFoodService.java
+++ src/main/java/com/bnj/restaurant/service/DefaultFoodService.java
@@ -1,4 +1,16 @@
 package com.bnj.restaurant.service;
 
+import com.bnj.restaurant.entity.Food;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service @Slf4j
 public class DefaultFoodService implements FoodService{
+    @Override
+    public List<Food> getFoods() {
+        log.debug("I am getFoods() in service");
+        return null;
+    }
 }
--- src/main/java/com/bnj/restaurant/service/FoodService.java
+++ src/main/java/com/bnj/restaurant/service/FoodService.java
@@ -1,4 +1,10 @@
 package com.bnj.restaurant.service;
 
+import com.bnj.restaurant.entity.Food;
+
+import java.util.List;
+
 public interface FoodService {
+
+    List<Food> getFoods();
 }
