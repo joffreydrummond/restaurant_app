d5ce64ca16fd964f612560ea3961489aedf36527
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-27 09:59:01 -0700

    created getFoods definition in FoodOperation. implemented method in FoodController. added slf4j and restcontroller annotations

--- src/main/java/com/bnj/restaurant/controller/FoodController.java
+++ src/main/java/com/bnj/restaurant/controller/FoodController.java
@@ -1,4 +1,18 @@
 package com.bnj.restaurant.controller;
 
+
+import com.bnj.restaurant.entity.Food;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j @RestController
 public class FoodController  implements FoodOperation{
+
+
+    @Override
+    public List<Food> getFoods() {
+        return null;
+    }
 }
--- src/main/java/com/bnj/restaurant/controller/FoodOperation.java
+++ src/main/java/com/bnj/restaurant/controller/FoodOperation.java
@@ -1,12 +1,43 @@
 package com.bnj.restaurant.controller;
 
+import com.bnj.restaurant.entity.Food;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 @RequestMapping("/v1/foods")
 @OpenAPIDefinition(
     info = @Info(title = "Food API"),
     servers = {@Server(url = "http://localhost:8080", description = "Local Server")})
-public interface FoodOperation {}
+public interface FoodOperation {
+    @Operation(
+            summary = "Returns a list of foods",
+            description = "Returns a list of foods",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "A list of foods is returned",
+                            content =
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Food.class)))
+            })
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    List<Food> getFoods();
+
+
+
+
+}
