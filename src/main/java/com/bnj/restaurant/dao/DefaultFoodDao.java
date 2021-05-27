c44f53b4107f9c1e502b6d8ab5e17de7eec55857
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-27 11:14:27 -0700

    added slf4j and component annotations to DefaultFoodDao

--- src/main/java/com/bnj/restaurant/dao/DefaultFoodDao.java
+++ src/main/java/com/bnj/restaurant/dao/DefaultFoodDao.java
@@ -1,14 +1,17 @@
 package com.bnj.restaurant.dao;
 
 import com.bnj.restaurant.entity.Food;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
+@Component
 public class DefaultFoodDao implements FoodDao {
 
-
-    @Override
-    public List<Food> getFoods() {
-        return null;
-    }
+  @Override
+  public List<Food> getFoods() {
+    return null;
+  }
 }
