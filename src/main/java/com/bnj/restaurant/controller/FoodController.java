626e0a74380005735656053adaf3523040759c5a
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-30 13:18:56 -0700

    empty array go away

--- src/main/java/com/bnj/restaurant/controller/FoodController.java
+++ src/main/java/com/bnj/restaurant/controller/FoodController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -20,14 +21,20 @@ public class FoodController implements FoodOperation {
   @Override
   public List<Food> getFoods(FoodTypes food_type) {
     log.debug("I am getFoods(food_type) in controller");
-    return foodService.getFoods(food_type);
+    List<Food> foodList = new ArrayList<>();
+    if (food_type != null) {
+      foodList = foodService.getFoods(food_type);
+    } else {
+      foodList = foodService.getFoods();
+    }
+    return foodList;
   }
 
-  @Override
-  public List<Food> getFoods() {
-    log.debug("I am getFoods() in controller");
-    return foodService.getFoods();
-  }
+//  @Override
+//  public List<Food> getFoods() {
+//    log.debug("I am getFoods() in controller");
+//    return foodService.getFoods();
+//  }
 
 
 
